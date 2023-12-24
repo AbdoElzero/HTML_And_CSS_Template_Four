32e198ba8afe389099613c1cebe54af41c05914d
AbdoElzero <127994249+AbdoElzero@users.noreply.github.com>
2023-12-24 18:11:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let icons = document.getElementsByClassName("delete")[0];
+let icon = document.getElementsByClassName("delete")[1];
+let ico = document.getElementsByClassName("delete")[2];
+let ic = document.getElementsByClassName("delete")[3];
+let i = document.getElementsByClassName("delete")[4];
+
+
+
+icons.onclick = function () {
+    icons.parentElement.remove()
+}
+icon.onclick = function () {
+    icon.parentElement.remove()
+}
+ico.onclick = function () {
+    ico.parentElement.remove()
+}
+ic.onclick = function () {
+    ic.parentElement.remove()
+}
+i.onclick = function () {
+    i.parentElement.remove()
+}
---
+++ js/two.js
@@ -0,0 +1,51 @@
+let a = document.getElementsByClassName("del")[0];
+let b = document.getElementsByClassName("del")[1];
+let c = document.getElementsByClassName("del")[2];
+let d = document.getElementsByClassName("del")[3];
+let e = document.getElementsByClassName("del")[4];
+let f = document.getElementsByClassName("del")[5];
+let g = document.getElementsByClassName("del")[6];
+let h = document.getElementsByClassName("del")[7];
+let i = document.getElementsByClassName("del")[8];
+let j = document.getElementsByClassName("del")[9];
+let good = document.getElementsByClassName("friend")[0];
+let good1 = document.getElementsByClassName("friend")[1];
+let good2 = document.getElementsByClassName("friend")[2];
+let good3 = document.getElementsByClassName("friend")[3];
+let good4 = document.getElementsByClassName("friend")[4];
+let good5 = document.getElementsByClassName("friend")[5];
+let good6 = document.getElementsByClassName("friend")[6];
+let good7 = document.getElementsByClassName("friend")[7];
+let good8 = document.getElementsByClassName("friend")[8];
+let good9 = document.getElementsByClassName("friend")[9];
+
+a.onclick= function () {
+    good.style.display = "none"
+}
+b.onclick= function () {
+    good1.style.display = "none"
+}
+c.onclick= function () {
+    good2.style.display = "none"
+}
+d.onclick= function () {
+    good3.style.display = "none"
+}
+e.onclick= function () {
+    good4.style.display = "none"
+}
+f.onclick= function () {
+    good5.style.display = "none"
+}
+g.onclick= function () {
+    good6.style.display = "none"
+}
+h.onclick= function () {
+    good7.style.display = "none"
+}
+i.onclick= function () {
+    good8.style.display = "none"
+}
+j.onclick= function () {
+    good9.style.display = "none"
+}
